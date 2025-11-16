7337a729956d5b16e3b69cfe51c246e63a6d3084
Anami-II <57529849+Anami-II@users.noreply.github.com>
2025-11-16 19:06:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi, esse é o clone do Tab News, seja bem vindo! 🔥</h1>;
+  return <h1>Oi, esse é o clone do Tab News, seja bem vindo! 🔥</h1>;
 }
 
 export default Home;
